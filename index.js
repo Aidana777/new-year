03c81cbef782783426e0a314717a4735b06af908
Aidana777 <aidananiazalieva27@gmail.com>
2022-01-10 13:55:27 +0600

    poop

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const btn=document.getElementById("btn")
-const box=document.getElementById("box")
-const boxbtn=document.getElementById("box-btn")
-btn.addEventListener("click",function() {
-    box.style.display= "block"
-})
+const btn = document.getElementById("btn");
+const box = document.getElementById("box");
+const boxbtn = document.getElementById("box-btn");
+btn.addEventListener("click", function () {
+    box.style.display = "block";
+});
 
-boxbtn.addEventListener("click",function() {
-    box.style.display= "none"
-})
-
-function myFunction(imgs) {
-    var expandImg = document.getElementById("expandedImg");
-    var imgText = document.getElementById("imgtext");
-    expandImg.src = imgs.src;
-    imgText.innerHTML = imgs.alt;
-    expandImg.parentElement.style.display = "block";
-}
+boxbtn.addEventListener("click", function () {
+    box.style.display = "none";
+});
+$(".gallery-list").magnificPopup({
+  delegate: "a",
+  type: "image",
+  gallery: {
+    enabled: true
+  }
+});
